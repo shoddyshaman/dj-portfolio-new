ab727da9a0635d741d99707ba77dd3de3b49c7a5
shoddyshaman <dhananjoy.tiwari@gmail.com>
2024-03-04 14:18:40 -0700

    download working

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -211,7 +211,16 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button
+        href="/resume-docx.docx"
+        variant="secondary"
+        className="group mt-6 w-full"
+        rel="noopener noreferrer"
+        target="_blank"
+        locale={false}
+        download
+      >
+      
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
